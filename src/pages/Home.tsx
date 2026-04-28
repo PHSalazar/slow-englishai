e7cfb59d1ee19c22884de70b354b629a7e81a298
PHSalazar <ph.s.salazar@gmail.com>
2026-04-28 20:54:32 -0300

    style: responsive layout Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,21 @@ import Card from "../components/Home/Card"
 
 const Home = () => {
     return (
-        <div className="flex flex-col gap-5 h-full">
+        <div className="flex flex-col gap-5">
             <h1 className="font-extrabold text-2xl">Bem-vindo de volta, João da Silva!</h1>
             <p className="text-xs font-light">Você está a <b>5</b> dias de completar sua meta semanal.</p>
 
-            <div className="grid grid-cols-6 grid-rows-14 gap-4 h-full">
-                <Card className="col-span-4 row-span-7 bg-white">
+            <div className="flex flex-col sm:grid sm:grid-cols-6 sm:grid-rows-14 gap-4">
+                <Card className="col-span-4 min-h-[200px] sm:row-span-7 bg-white">
                     Lorem ipsum dolor sit amet.
                     Next Lesson
                 </Card>
-                <Card className="col-span-2 row-span-7 bg-white">
+                <Card className="col-span-2 min-h-[200px] sm:row-span-7 bg-white">
                     Lorem, ipsum dolor.
                     Last Info
                 </Card>
 
-                <Card className="col-span-6 row-span-7">
+                <Card className="col-span-6 min-h-[200px] sm:row-span-7">
                     Lorem ipsum dolor sit amet.
                     Quick Reply/Quiz
                 </Card>
